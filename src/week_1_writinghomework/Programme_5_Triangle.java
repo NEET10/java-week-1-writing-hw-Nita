0beb0bcc6608a191d969634ad1146cd1de2dac12
Nita0310 <neetgabani@gmail.com>
2023-03-17 19:39:07 +0000

    My java week 1 writing homework

---
+++ src/week_1_writinghomework/Programme_5_Triangle.java
@@ -0,0 +1,21 @@
+package week_1_writinghomework;
+
+/**
+ * 5. Write a Java program to print below triangle
+ *           *
+ *          * *
+ *         * * *
+ *        * * * *
+ *       * * * * *
+ *      * * * * * *
+ */
+public class Programme_5_Triangle {
+    public static void main(String[] args) {
+        System.out.println("       *       ");
+        System.out.println("      * *      ");
+        System.out.println("     * * *     ");
+        System.out.println("    * * * *    ");
+        System.out.println("   * * * * *   ");
+        System.out.println("  * * * * * *  ");
+    }
+}
